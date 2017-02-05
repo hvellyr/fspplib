e9699913c55db1f3ea0a39e52512b027babfd8e6
hvellyr <gck@eyestep.org>
2017-02-05 13:21:11 +0100

    Use STL make_unique if available

--- src/include/fspp/estd/memory.hpp
+++ src/include/fspp/estd/memory.hpp
@@ -14,6 +14,8 @@
 namespace eyestep {
 namespace estd {
 
+#if !defined(FSPP_HAVE_STD_MAKE_UNIQUE)
+
 template <class T, class... Args>
 auto
 make_unique(Args&&... args) -> enable_if_t<!std::is_array<T>::value, std::unique_ptr<T>>
@@ -27,6 +29,12 @@ make_unique(std::size_t size) -> enable_if_t<std::is_array<T>::value, std::uniqu
 {
   return std::unique_ptr<T>{new typename std::remove_extent<T>::type[size]()};
 }
+
+#else
+
+using std::make_unique;
+
+#endif
 
 }  // namespace estd
 }  // namespace eyestep
